95a8c3cbfe152ede3ba2a046f3fbc0b3b3ab0bdb
Saaketh <123851156+gintokia@users.noreply.github.com>
2023-01-29 18:06:40 -0500

    Update GetUIData.java

--- cryptex/src/main/java/cryptoTrader/gui/GetUIData.java
+++ cryptex/src/main/java/cryptoTrader/gui/GetUIData.java
@@ -1,1 +1,12 @@
+/**
+ * The GetUIData interface provides a method for logging in
+ * 
+ * @author Saaketh Chenna
+ */
+package cryptoTrader.gui;
 
+public interface GetUIData {
+	
+	public void login();
+
+}
